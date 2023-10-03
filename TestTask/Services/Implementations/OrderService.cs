6839293b019b7591ed606b522b2fb467460d7656
nikitayashny <95637235+nikitayashny@users.noreply.github.com>
2023-10-03 20:03:55 +0300

    iFortex task completed

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+namespace TestTask.Services.Implementations;
+
+public class OrderService : IOrderService
+{
+    private readonly ApplicationDbContext _context;
+
+    public OrderService(ApplicationDbContext context) => _context = context;
+
+    public async Task<Order> GetOrder() =>
+        await _context.Orders.OrderByDescending(o => o.Price)   // сортируем по полю Price в порядке убывания
+        .FirstOrDefaultAsync(); // берём первую запись
+
+    public async Task<List<Order>> GetOrders() =>
+        await _context.Orders.Where(o => o.Quantity > 10) // Фильтруем записи по полю Quantity, где значение больше 10
+        .ToListAsync(); 
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+namespace TestTask.Services.Implementations;
+
+public class UserService : IUserService
+{
+    private readonly ApplicationDbContext _context;
+
+    public UserService(ApplicationDbContext context) => _context = context;
+
+    public async Task<User> GetUser()
+    {
+        var userWithOrdersCount = await _context.Orders
+            .GroupBy(o => o.UserId) // группируем записи по UserId
+            .Select(u => new    // подсчитываем количество заказов для каждого пользователя
+            {
+                UserId = u.Key,
+                OrdersCount = u.Count()
+            })
+            .OrderByDescending(u => u.OrdersCount)  // сортируем по убыванию
+            .FirstOrDefaultAsync(); // берём первую запись
+
+        var user = await _context.Users // фильтруем записи по полю Id, равному UserId пользователя с наибольшим количеством заказов
+            .Where(u => u.Id
+            .Equals(userWithOrdersCount.UserId))
+            .FirstOrDefaultAsync();
+
+        return user;
+    }
+
+    public async Task<List<User>> GetUsers() =>
+        await _context.Users
+            .Where(u => u.Status
+            .Equals(UserStatus.Inactive))   // выбираем неактивных пользователей
+            .ToListAsync();
+}
